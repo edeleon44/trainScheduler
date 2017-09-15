be921bf693594dc3e80409002c0913e346ea1791
Edward DeLeon Jr <edwarddeleon@Edwards-MBP.home>
2017-09-15 14:51:58 -0500

    updates

--- assets/script.js
+++ assets/script.js
@@ -16,6 +16,7 @@
   var name = "";
   var destination = "";
   var frequency = "";
+  var firstTrain = "";
 
   //Capture Button Clicks
   $('#add-train').on("click", function(event){
@@ -26,14 +27,17 @@
   name = $("#name-input").val().trim();
   destination = $("#destination-input").val().trim();
   frequency = $("#frequency-input").val().trim();
+  firstTrain = $("#firstTrain-input").val().trim();
 
   //Code to handle the push 
-  database.ref().push({
-  	name: name,
-  	destination: destination,
-  	frequency: frequency,
-  	dateAdded: firebase.database.ServerValue.TIMESTAMP
-  });
+    var objToPush = {
+      name: name,
+      destination: destination,
+      frequency: frequency,
+      firstTrain: firstTrain,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+}
+database.ref().push(objToPush)
 });
 
   //FireBase Watcher (watch the changes w/o the overhead) + order/limit
@@ -41,15 +45,38 @@
   database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
   	//Storing the snapshot.val() in a var for convenience
   	var sv = snapshot.val();
-
+    console.log(sv);
   	// Check to see if the entry's are working
-  	console.log(sv.name);
-  	console.log(sv.destination);
-  	console.log(sv.frequency);
+  	//console.log(sv.name);
+  	//console.log(sv.destination);
+  	//console.log(sv.frequency);
 
-  	$("#name-display").html(sv.name);
-  	$("#destination-display").html(sv.destination);
-  	$("#frequency-display").html(sv.frequency);
+    //Moment.js calculations -->
+   
+
+    //var date = firstTrain;
+    //var arrival = moment(date).add(frequency, 'mins').format('1');
+
+
+    $("#name-display").append("<p>" + sv.name + "</p>");
+    $("#destination-display").append("<p>" + sv.destination + "<p>");
+    $("#firstTrain-display").append("<p>" + sv.firstTrain + "</p>");
+    $("#frequency-display").append("<p>" + sv.frequency + "</p>");
+    $("#arrival-display").append("<p>" + moment(nextTrain).format("hh:MM") + "</p>");
+    $("#away-display").append("<p>" + minUntilTrain + "</p>")
+    
+
+
+    var firstTrainMoment = moment(snapshot.val().firstTrain, "hh:MM").subtract(1, "years");
+    var diffTime = moment().diff(moment(firstTrainMoment), "minutes");
+    var remainder = diffTime % snapshot.val().frequency;
+    var minUntilTrain = snapshot.val().frequency - remainder;
+    var nextTrain = moment().add(minUntilTrain, "minutes");
+
+    
+
+
+
 
   }), function(errorObject) {
   	console.log("Error handled: " + errorObject.code);
